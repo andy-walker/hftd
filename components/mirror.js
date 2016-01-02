47f96141db831936f761fc1bfe3df46f94c5d8b5
andy-walker <andyw+github2@circle-interactive.co.uk>
2016-01-02 23:49:58 +0000

    functions for trade mirroring

--- components/mirror.js
+++ components/mirror.js
@@ -2,11 +2,51 @@ var mirror = function() {
 
     var mirror = this;
 
-    mirror.start = function(callback) {
-        var accounts = hftd.config.mirrorAccounts;
-        for (var accountName in accounts) {
-            var account = accounts[accountName];
+    mirror.client   = [];
+    mirror.trades   = {};
+    mirror.balances = {};
+
+    mirror.onEvent = function(event) {
+
+        var transaction = event.transaction;
+
+        switch (transaction.type) {
+            
+            case 'STOP_LOSS_FILLED':
+            case 'TAKE_PROFIT_FILLED':
+                
+                var type = (transaction.type == 'STOP_LOSS_FILLED' ? 'stop loss' : 'take profit');
+                hftd.log(sprintf('Closed trade %s on %s (%s filled)', transaction.tradeId, transaction.instrument, type));
+
+                mirror.balances[transaction.accountId] = transaction.accountBalance; 
+                delete mirror.trades[transaction.tradeId];
+                break;
+
         }
+
+    };
+
+    mirror.start = function(callback) {
+        
+        var accounts = hftd.config.mirrorAccounts;
+        
+        for (var accountName in accounts) {
+            
+            var account     = accounts[accountName];
+            var environment = account.accountType == 'demo' ? 'practice' : 'live';
+            var client      = new OANDAAdapter({
+                environment: environment,
+                accessToken: account.accessToken
+            });
+
+            client.subscribeEvents(function(event) {
+                mirror.onEvent(event);
+            });
+
+            mirror.client.push(client);
+
+        }
+    
     };
 
     mirror.trade = function(strategy, params) {
