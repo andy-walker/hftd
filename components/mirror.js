095603211704e2cc069350cbf574fea8dbafa7c3
andy-walker <andyw+github2@circle-interactive.co.uk>
2016-01-20 22:43:37 +0000

    work on mirroring

--- components/mirror.js
+++ components/mirror.js
@@ -1,10 +1,133 @@
+var OANDAAdapter = require('oanda-adapter');
+
 var mirror = function() {
 
     var mirror = this;
 
-    mirror.client   = [];
+    mirror.dataFile = __dirname + '/../data/mirror/trades.json';
+
+    mirror.client   = {};
     mirror.trades   = {};
-    mirror.balances = {};
+    mirror.accounts = {};
+
+    mirror.closeTrades = function(masterTradeId, jobCallback) {
+        
+        if (!(masterTradeId in mirror.trades))
+            return;
+
+        // iterate over linked trades for the master id ..
+        async.forEach(Object.keys(mirror.trades[masterTradeId]), function(accountName, taskCallback) {
+
+            var tradeId = mirror.trades[masterTradeId][accountName]
+            var client  = mirror.client[accountName];
+
+            var accountId = mirror.getAccountId(accountName);
+            if (!accountId)
+                return hftd.error(sprintf("Unable to get account id for '%s' during mirror trade close", trade.accountName));
+
+            client.closeTrade(accountId, tradeId, function(error, confirmation) {
+                
+                if (error) {
+                    
+                    hftd.error(error);
+                    
+                    if (error == 'Trade not found') {
+                        hftd.log('Removing local entry');
+                        delete mirror.trades[masterTradeId][accountName];
+                    }
+
+                } else {
+
+                    if (masterTradeId in mirror.trades && accountName in mirror.trades[masterTradeId]) {
+
+                        // not sure if we want to archive mirrored trades or not? probably at some point
+                        // hftd.strategist.archiveTrade({tradeId: tradeId}, execution.trades[tradeId]);
+                        delete mirror.trades[masterTradeId][accountName];
+
+                    }
+
+                    hftd.log(sprintf("Mirrored position closed (%s on '%s') - [ %s ]", tradeId, accountName, color('OK', 'green')));
+
+                }
+
+                if (!Object.keys(mirror.trades[masterTradeId]).length)
+                    delete mirror.trades[masterTradeId];
+            
+            }); 
+
+        }, function(error) {
+            // commit data to disk regardless of whether there was an error or not.
+            // we'll trust this to work for the moment, remove the need for another nested callback
+            mirror.saveData(); 
+            if (jobCallback)
+                jobCallback(error);
+        });
+    
+
+    };
+
+    /**
+     * Retrieve account stats (balance, marginUsed etc) by accountName
+     */
+    mirror.getAccount = function(accountName) {
+        if (accountName in mirror.accounts)
+            return mirror.accounts[accountName];
+        return false;
+    };
+
+    /**
+     * Get the account id for a named account
+     */
+    mirror.getAccountId = function(accountName) {
+        var accounts = hftd.config.mirrorAccounts;
+        if (accountName in accounts && 'accountId' in accounts[accountName])
+            return accounts[accountName].accountId;
+        return false;      
+    };
+
+    /**
+     * Get accounts to mirror onto and associated multipliers based on 
+     * strategy - return false if mirroring disabled / not configured
+     */
+    mirror.getAccountMultipliers = function(strategy) {
+        
+        if ('mirror' in hftd.config.strategies[strategy]) {
+            
+            var mirror      = hftd.config.strategies[strategy].mirror;
+            var multipliers = {};
+
+            for (var accountName in mirror) {
+                multipliers[accountName] = mirror[accountName];
+            }
+            
+            if (Object.keys(multipliers).length)
+                return multipliers;
+
+        }
+
+        return false;
+    
+    };
+
+    /**
+     * Search trades data for masterTradeId when supplied with a tradeId
+     */
+    mirror.getMasterTradeId = function(tradeId) {
+        for (var masterTradeId in mirror.trades) {
+            for (var accountName in mirror.trades[masterTradeId]) {
+                if (mirror.trades[masterTradeId][accountName] == tradeId)
+                    return masterTradeId;
+            }
+        }
+        return false;
+    };
+
+    /**
+     * Determine if mirroring enabled for a given strategy
+     */
+    mirror.isEnabled = function(strategy) {
+        return ('mirror' in hftd.config.strategies[strategy]);
+    };
 
     mirror.onEvent = function(event) {
 
@@ -14,26 +137,148 @@ var mirror = function() {
             
             case 'STOP_LOSS_FILLED':
             case 'TAKE_PROFIT_FILLED':
+            case 'TRADE_CLOSE':
                 
                 var reason = {
                     'STOP_LOSS_FILLED':   'hit stop loss',
                     'TAKE_PROFIT_FILLED': 'hit take profit',
-                    'TRADE_CLOSED':       'trade closed'
+                    'TRADE_CLOSE':        'trade closed'
                 }[transaction.type];
 
                 hftd.log(sprintf('Closed trade %s on %s (%s)', transaction.tradeId, transaction.instrument, reason));
 
-                mirror.balances[transaction.accountId] = transaction.accountBalance; 
-                delete mirror.trades[transaction.tradeId];
+                //mirror.balances[transaction.accountId] = transaction.accountBalance; 
+                var masterTradeId = mirror.getMasterTradeId(transaction.tradeId);
+
+                if (!masterTradeId)
+                    return hftd.warning("Master trade id not found for trade id " + transaction.tradeId);
+
+                for (var accountName in mirror.trades[masterTradeId]) {
+                    if (mirror.trades[masterTradeId] == transaction.tradeId) {
+                        delete mirror.trades[masterTradeId][accountName];
+                        if (!Object.keys(mirror.trades[masterTradeId]).length)
+                            delete mirror.trades[masterTradeId];   
+                        mirror.saveData();            
+                    }
+                }
+
+
                 break;
 
         }
 
     };
 
+    mirror.openTrade = function(accountName, masterTradeId, params) {
+
+        var client    = mirror.client[accountName];
+        var accountId = mirror.getAccountId(accountName);
+
+        params.type = 'market';
+
+        client.createOrder(accountId, params, function(error, confirmation) {
+            
+            if (error)
+                return hftd.error(error);
+
+            if (typeof confirmation.tradeOpened.id !== 'undefined') {
+                
+                if (!(masterTradeId in mirror.trades))
+                    mirror.trades[masterTradeId] = {};
+                
+                mirror.trades[masterTradeId][accountName] = confirmation.tradeOpened.id;
+                mirror.saveData();
+
+            } else {
+                hftd.error(sprintf('Unable to open %s trade on %s', params.instrument, accountName));
+                console.log(confirmation);
+            }
+
+        });
+
+    };
+
+    mirror.openTrades = function(params) {
+
+        var multipliers = mirror.getAccountMultipliers(params.strategy);
+        var config      = hftd.config.strategies[params.strategy];
+        var execution   = hftd.execution;
+        var chartist    = hftd.chartist;
+
+        if (!multipliers)
+            return;
+
+        for (var accountName in multipliers) {
+            
+            var multiplier = multipliers[accountName];
+            var account    = mirror.getAccount(accountName);
+
+            if (!account) {
+                hftd.warning(sprintf("Unable to load account details for '%'", accountName));
+                continue;
+            }
+
+            // if multiplier is zero, indicates a disabled state - skip account
+            if (!multiplier)
+                continue;
+
+            var instrument = params.instrument;
+            var quote      = chartist.getQuote(instrument);
+
+            if (params.side == 'buy') {
+
+                // prepare to open long position
+                var quotePrice    = quote.ask;
+                var oppositePrice = quote.bid;
+                var gbpAmount     = account.balance * ((config.positionSize * multiplier) / 100);
+                var units         = Math.floor(chartist.convert(gbpAmount, 'GBP', instrument.split('_')[0]) * 20);
+                var stopLoss      = execution.calculateStopLoss('buy', oppositePrice, config.stopLoss);
+
+            } else if (params.side == 'sell') {
+
+                // open short position
+                var quotePrice    = quote.bid;
+                var oppositePrice = quote.ask;
+                var gbpAmount     = account.balance * ((config.positionSize * multiplier) / 100);
+                var units         = Math.floor(chartist.convert(gbpAmount, 'GBP', instrument.split('_')[0]) * 20);
+                var stopLoss      = execution.calculateStopLoss('sell', oppositePrice, config.stopLoss);
+
+            }
+
+            // adjust stop-loss to correct precision
+            var precision = execution.getPipPrecision(instrument);
+            stopLoss      = stopLoss.toFixed(precision);
+
+            mirror.openTrade(accountName, params.id, {
+                side:       params.side,
+                instrument: instrument,
+                units:      units,
+                stopLoss:   stopLoss
+            });
+
+        }
+
+    };
+
+    /**
+     * Save internal data to disk
+     */
+    mirror.saveData = function(callback) {
+        fs.writeFile(mirror.dataFile, JSON.stringify(mirror.trades), function(error) {
+            if (error)
+                hftd.error(error);
+            if (callback)
+                return callback(error);
+        });       
+    };
+
+    /**
+     * Initialize component
+     */
     mirror.start = function(callback) {
         
-        var accounts = hftd.config.mirrorAccounts;
+        var accounts  = hftd.config.mirrorAccounts;
+        mirror.trades = require(mirror.dataFile); 
         
         for (var accountName in accounts) {
             
@@ -48,20 +293,32 @@ var mirror = function() {
                 mirror.onEvent(event);
             });
 
-            mirror.client.push(client);
+            mirror.client[accountName] = client;
 
         }
     
+        // update accounts (balances etc) now, and 20 seconds past each minute
+        mirror.updateAccounts();
+        hftd.scheduler.addJob("20 * * * * *", mirror.updateAccounts);
+        callback();
+
     };
 
-    mirror.trade = function(params) {
+    mirror.updateAccounts = function() {
 
-        var accounts = mirror.getAccounts(params.strategy);
-
-        if (!accounts)
-            return;
-
-        
+        var accounts = hftd.config.mirrorAccounts;
+        async.forEach(Object.keys(accounts), function(accountName, taskCallback) {
+            var accountId = accounts[accountName].accountId;
+            mirror.client[accountName].getAccount(accountId, function(error, account) {
+                if (error)
+                    return taskCallback(error);
+                mirror.accounts[accountName] = account;
+                taskCallback();
+            });
+        }, function(error) {
+            if (error)
+                return hftd.error(error);
+        });
 
     };
 
--- components/stream-api.js
+++ components/stream-api.js
@@ -16,8 +16,6 @@ streamAPI.prototype.start = function(callback) {
     var streamAPI   = hftd.streamAPI;
     var instruments = streamAPI.getInstrumentList();
     
-    //instruments = ["AUD_CAD","AUD_CHF","AUD_HKD","AUD_JPY","AUD_NZD","AUD_SGD","AUD_USD","BCO_USD","CAD_CHF","CAD_HKD","CAD_JPY","CAD_SGD","CHF_HKD","CHF_JPY","CHF_ZAR","CORN_USD","EUR_AUD","EUR_CAD","EUR_CHF","EUR_CZK","EUR_DKK","EUR_GBP","EUR_HKD","EUR_HUF","EUR_JPY","EUR_NOK","EUR_NZD","EUR_PLN","EUR_SEK","EUR_SGD","EUR_TRY","EUR_USD","EUR_ZAR","GBP_AUD","GBP_CAD","GBP_CHF","GBP_HKD","GBP_JPY","GBP_NZD","GBP_PLN","GBP_SGD","GBP_USD","GBP_ZAR","HKD_JPY","NATGAS_USD","NZD_CAD","NZD_CHF","NZD_HKD","NZD_JPY","NZD_SGD","NZD_USD","SGD_CHF","SGD_HKD","SGD_JPY","SOYBN_USD","SUGAR_USD","TRY_JPY","USD_CAD","USD_CHF","USD_CZK","USD_DKK","USD_HKD","USD_HUF","USD_INR","USD_JPY","USD_MXN","USD_NOK","USD_PLN","USD_SAR","USD_SEK","USD_SGD","USD_THB","USD_TRY","USD_TWD","USD_ZAR","WHEAT_USD","WTICO_USD","XAG_AUD","XAG_CAD","XAG_CHF","XAG_EUR","XAG_GBP","XAG_HKD","XAG_JPY","XAG_NZD","XAG_SGD","XAG_USD","XAU_AUD","XAU_CAD","XAU_CHF","XAU_EUR","XAU_GBP","XAU_HKD","XAU_JPY","XAU_NZD","XAU_SGD","XAU_USD","XAU_XAG","XCU_USD","XPD_USD","ZAR_JPY","UK100_GBP","US30_USD","SPX500_USD","HK33_HKD","NAS100_USD","DE30_EUR","JP225_USD" ];  
-
     if (!instruments.length)
         return callback('No instruments found to subscribe to.');
     
@@ -33,6 +31,8 @@ streamAPI.prototype.start = function(callback) {
     if (splitInstruments[1].length)
         streamAPI.rateStream.B = new streamAPI.createRateStream(splitInstruments[1], streamAPI.onTickUpdate);
     
+    hftd.log(sprintf('Subscribed to %d instruments - [ %s ]', instruments.length, color('OK', 'green')));
+
     hftd.log('Establishing Oanda event stream connection ...');
     hftd.restAPI.client.subscribeEvents(function(event) {
         hftd.execution.onEvent(event);
