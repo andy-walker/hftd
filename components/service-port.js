e24ee54c2b7d5c6b12d46c7a895627da83ee1448
andy-walker <andyw+github2@circle-interactive.co.uk>
2016-01-30 23:19:07 +0000

    json-rpc server + client

--- components/service-port.js
+++ components/service-port.js
@@ -1,8 +1,83 @@
-module.exports = function() {
+/**
+ * Service port component
+ * Run a json-rpc service for executing commands via a client
+ */
+var servicePort = function() {
 
-    this.rpc    = require('node-json-rpc');
-    this.server = new rpc.Server({
+    var servicePort = this;
+
+    servicePort.server = {};
+
+    /**
+     * Add command information about commands to this object
+     */
+    servicePort.commandInfo = {
         
-    });
+        accountInfo: {
+            description: "Get account information for the specified account",
+            usage:       "account-info <account name>"
+        }
 
+    };
+
+    servicePort.accountInfo = function(params, callback) {
+
+        hftd.log('Retrieving account info for ' + params.account, 'command');
+        
+        if ('mirror' in hftd && params.account in hftd.mirror.accounts) 
+            return callback(null, {
+                message:   sprintf("Account information for '%s'", 
+                data:      hftd.mirror.accounts[params.account]),
+                formatter: "labelValue"
+            }); 
+        
+
+            
+    };
+
+    /**
+     * Initialize component
+     */
+    servicePort.start = function(callback) {
+
+        if ('servicePort' in hftd.config) {
+
+            servicePort.config = hftd.config.servicePort;
+
+            if (servicePort.config.enabled) {
+
+                hftd.log('Initializing service port ...');
+
+                var rpc    = require('node-json-rpc');
+                var server = new rpc.Server(servicePort.config.options);
+
+                // add method for each of the commands in commandInfo
+                for (command in servicePort.commandInfo)
+                    server.addMethod(command, servicePort[command]);
+
+                // start the server
+                server.start(function(error) {
+                    
+                    // fail ..
+                    if (error) {
+                        hftd.error('Service port failed to initialize.');
+                        return callback(error);
+                    }
+                    
+                    // success ..
+                    hftd.log(sprintf('Service port initialized - [ %s ]', color('OK', 'green')));
+                    callback();
+                
+                });
+
+            } else return callback(); // not enabled
+
+        } else return callback(); // not configured
+
+    };
+
+};
+
+module.exports = function() {
+    return new servicePort();
 };
--- hft.d
+++ hft.d
@@ -11,15 +11,18 @@ require('./components/console');
 
 hftd.initialized = false;
 
+hftd.config      = new require(__dirname + '/config/config.json');
+hftd.db          = new require('./components/db')(hftd.config.db);
 hftd.restAPI     = new require('./components/rest-api')();
 hftd.streamAPI   = new require('./components/stream-api')();
 
-hftd.config      = new require(__dirname + '/config/config.json');
 hftd.chartist    = new require('./components/chartist')();
 hftd.scheduler   = new require('./components/scheduler')();
 hftd.execution   = new require('./components/execution')();
 hftd.servicePort = new require('./components/service-port')(); 
 hftd.strategist  = new require('./components/strategist')();
+//hftd.virtual     = new require('./components/virtual')();
+
 
 // construct a list of initialization tasks
 var initTasks = [
@@ -39,10 +42,13 @@ if (typeof hftd.config.mirrorEnabled !== 'undefined' && hftd.config.mirrorEnable
     initTasks.push(hftd.mirror.start);
 }
 
-// start strategist component last
+// start json-rpc server
+initTasks.push(hftd.servicePort.start);
+
+// start / initialize strategies last, once all other components have been loaded
 initTasks.push(hftd.strategist.start);
 
-// run init tasks list using async.series
+// run initialzation tasks in series
 async.series(initTasks, function(error) {
     if (error)
         hftd.log(error, 'error');
